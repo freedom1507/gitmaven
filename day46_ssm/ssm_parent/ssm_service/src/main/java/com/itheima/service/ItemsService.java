3d4203a69dff637872fdddb1c654ca47c83a0fd2
freedom <2834250529@qq.com>
2021-09-15 23:46:05 +0800

    IDEA使用git进行版本控制

---
+++ day46_ssm/ssm_parent/ssm_service/src/main/java/com/itheima/service/ItemsService.java
@@ -0,0 +1,22 @@
+package com.itheima.service;
+
+import com.itheima.pojo.Items;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface ItemsService {
+    /***
+     * 列表查询
+     * @return
+     */
+    List<Items> findAll();
+
+    /***
+     * 增加商品
+     * @param items
+     * @return
+     */
+    int save(Items items);
+}
---
+++ day46_ssm/ssm_parent/ssm_service/src/test/java/com/itheima/ServiceTest.java
@@ -0,0 +1,21 @@
+package com.itheima;
+
+import com.itheima.pojo.Items;
+import com.itheima.service.ItemsService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ServiceTest {
+
+    public static void main(String[] args) {
+        //1）创建容器对象
+        ApplicationContext act = new ClassPathXmlApplicationContext("spring.xml");
+        //2）从容器中获取ItemService代理对象
+        ItemsService itemsService = act.getBean(ItemsService.class);
+
+        //3）执行增加操作
+        Items items = new Items();
+        items.setName("测试事务66778899");
+        itemsService.save(items);
+    }
+}
